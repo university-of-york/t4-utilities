4bbea4bc5dd8aaf46b81dea4bdce03428bd9fcf1
Chris Marsh <chris.marsh@epiphanysolutions.co.uk>
2018-02-28 10:32:54 +0000

    Set up basic Grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        force:true,
+        evil:true,
+        moz:true,
+        sub:true,
+        rhino:true
+      },
+      files: ['src/**/*.js']
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+    jsdoc: {
+      dist: {
+        src: ['src/**/*.js'],
+        options: {
+          destination: 'doc'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jsdoc');
+
+  grunt.registerTask('default', ['jshint', 'jsdoc']);
+
+};
