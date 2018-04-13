43851ac500a542c87571a2127ed0d85d6264555b
Dave Hill <dave.hill@outlook.com>
2018-04-13 16:31:29 +0100

    Mocha working

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,19 @@ module.exports = function(grunt) {
         }
       }
     },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          captureFile: 'results.txt', // Optionally capture the reporter output to a file
+          quiet: false, // Optionally suppress output to standard out (defaults to false)
+          clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
+          clearCacheFilter: (key) => true, // Optionally defines which files should keep in cache
+          noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
+        },
+        src: ['test/**/*.js']
+      }
+    },
     eslint: {
        options: {
            configFile: '.eslintrc.json'
@@ -24,7 +37,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("gruntify-eslint");
   // grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-jsdoc');
-
-  grunt.registerTask('default', ['eslint', 'jsdoc']);
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.registerTask('default', ['eslint', 'jsdoc', 'mochaTest']);
 
 };
