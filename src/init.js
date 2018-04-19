3edd4948e9a63adbc184b06bae8f6f0a04b692bb
Chris Marsh <chris.marsh@york.ac.uk>
2018-04-19 09:16:49 +0100

    Added PL logging to init function

--- src/init.js
+++ src/init.js
@@ -14,8 +14,22 @@ init: function (node) {
       this.init(node[i]);
     }
   }
-  if (this === node) {
-    delete this.init;
-  }
+    if (this === node) {
+      // This should be called once when a PL initialises
+      // Get the content type and content layout
+      importPackage(com.terminalfour.template);
+      importPackage(com.terminalfour.sitemanager);
+      try {
+        var tid = content.getTemplateID();
+        var tempManager = com.terminalfour.template.TemplateManager.getManager();
+        var templateName = tempManager.getName(dbStatement, tid);
+        this.console.log("templateName is "+templateName);
+        this.writeLog("pl", "init called by "+templateName+" using ???");
+      } catch(e) {
+        this.console.log("Error thrown");
+      }
+
+      delete this.init;
+    }
   return this;
 },
